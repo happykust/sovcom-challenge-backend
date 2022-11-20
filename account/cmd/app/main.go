cb7708c385d2ef75b1f3c03ce80c610a70edeaad
Ivan <Mrstarfox29@yandex.ru>
2022-11-20 09:10:56 +0300

    apia

--- account/cmd/app/main.go
+++ account/cmd/app/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"account/internal/domain/auth"
 	"account/internal/router"
 	"account/pkg/core/config"
 	"account/pkg/core/database"
 	"account/pkg/core/database/migrations"
+	"fmt"
 )
 
 func init() {
@@ -14,6 +16,17 @@ func init() {
 func main() {
 	database.OpenPostgresConnect()
 	migrations.InitMigrations()
+	//a := auth.SingUp(account.AccountSignUpRequest{Username: "lofSdax", Password: "lox", Email: "MrstarSfox29@yandex.ru", FirstName: "dfg", LastName: "dfg", ReferralCode: "90"})
+	//fmt.Println(a)
+	//gf := auth.SingIn("MrstarSfox29@yandex.ru", "lox")
+	//fmt.Println(gf)
+	//b := auth.VerifyUserRequest(account.AccountVerifyRequest{Id: 1, AdditionalContact: "23423234"})
+	//fmt.Println(b)
+	//c := auth.GetVerifyUserStatus(1)
+	//fmt.Println(c)
+	//auth.VerifyUser(1, auth.RegistrationStatusVerified)
+	t := auth.SingIn("MrstarSfox29@yandex.ru", "lox")
+	fmt.Println(t)
 	go router.AmqpMainRouter()
 	select {}
 
